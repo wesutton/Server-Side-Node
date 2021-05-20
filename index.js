5e0ce6131a61025b9ff448bb8c08f9c2af3322d0
wesutton <wesutton715@gmail.com>
2021-05-20 17:40:33 -0400

    sign in and database

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+let express = require("express")
 const app = express();
+const sequelize = require("./db")
+const user = require("./controllers/usercontroller");
 
+sequelize.sync();
 
-				
-app.use(express.json());
+app.use(express.json())
+app.use('/user', user);
+
 				
 app.listen(3000,()=> console.log("Listening on port 3000"));
