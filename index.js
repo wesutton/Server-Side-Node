eb1c0f196548e8bfd012c1aed59c2bbbc2cf53ca
wesutton <wesutton715@gmail.com>
2021-05-25 16:02:33 -0400

    second sign in push

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const sequelize = require("./db")
 const user = require("./controllers/usercontroller");
 
 sequelize.sync();
+app.use(require('./middleware/headers'));
 
 app.use(express.json())
 app.use('/user', user);
+
 
 				
 app.listen(3000,()=> console.log("Listening on port 3000"));
